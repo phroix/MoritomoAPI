41b01b174af17281b72578b793b4664370b7071b
phroix <94416893+phroix@users.noreply.github.com>
2025-07-21 00:52:00 +0200

    moritomo index changes

--- index.ts
+++ index.ts
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // home route
 app.get("/", (req, res) => {
-  res.json({ message: "KebapGuide-API." });
+  res.json({ message: "MoriTomo-API." });
 });
 
 
